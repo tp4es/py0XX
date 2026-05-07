f21fe5765c95077ddd588910a0554b6c906063c6
Tiago_laptop <tide-oli@student.42madrid.com>
2026-05-07 16:05:06 +0200

    ex0

--- py05/ex0/data_processor.py
+++ py05/ex0/data_processor.py
@@ -4,14 +4,10 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 
-from abc import ABC, abstractmethod
-from typing import Any
-
-
 class DataProcessor(ABC):
 
     def __init__(self):
-        self._data = []
+        self._data = list()
         self._counter = 0
 
     @abstractmethod
@@ -68,6 +64,7 @@ class TextProcessor(DataProcessor):
         else:
             self._data.append(data)
 
+
 class LogProcessor(DataProcessor):
 
     def validate(self, data: Any) -> bool:
@@ -93,8 +90,30 @@ class LogProcessor(DataProcessor):
         else:
             self._data.append(str(data))
 
+
+def function_test(obj: DataProcessor, list: list) -> None:
+    print(f"\nStart Testing {type(obj).__name__}")
+    for data in list:
+        try:
+            print(f"Trying to ingest Data: {data}, Valide DATA: {obj.validate(data)}")
+            obj.ingest(data)
+        except Exception as e:
+            print(f"Error: {e}")
+        print(f"Currently ingested: {obj._data}")
+    print("\n===Output DATA ===")
+    while len(obj._data) > 0:
+        print(obj.output())
+
+
 def data_processor():
-    pass
+    data_list = [1.01, 2.02, [3.03, 4], "hello",
+                 ["hello", "world"], {"dict": "value"}]
+    numeric = NumericProcessor()
+    text = TextProcessor()
+    log = LogProcessor()
+    function_test(numeric, data_list)
+    function_test(text, data_list)
+    function_test(log, data_list)
 
 
 if __name__ == "__main__":
