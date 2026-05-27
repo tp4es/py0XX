86a05882885b003e5fa60338f46871434f99525a
Tiago_laptop <tide-oli@student.42madrid.com>
2026-05-27 18:27:28 +0200

    recipes:partial

--- py06/alchemy/transmutation/recipyes.py
+++ py06/alchemy/transmutation/recipyes.py
@@ -1,1 +1,10 @@
+import alchemy
+from elements import create_fire
 
+
+def lead_to_gold() -> str:
+    return (
+        "Recibe transmuting Lead to Gold: "
+        f"brew '{alchemy.create_air()}' and '{alchemy.original_strength()}' mixed with "
+        f"{create_fire}"
+    )
