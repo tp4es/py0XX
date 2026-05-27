bd671c9fb00288fe49a4a8b4fb13de0c76224e04
Tiago_laptop <tide-oli@student.42madrid.com>
2026-05-27 16:25:02 +0200

    potions updated

--- py06/alchemy/potions.py
+++ py06/alchemy/potions.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env	python3
+from alchemy import create_air
+from alchemy import create_earth
+from ..elements import create_water, create_fire
 
+elements = []
+
+
+def healing_potion() -> str:
+    try:
+        elements.append(create_air(), create_earth())
+    except AttributeError as e:
+        print(f"Error Found: {e}")
+    return (
+        f"Healing potion brewed with '{elements[0]}' and"
+        f" '{elements[1]}'"
+    )
+
+
+def strength_potion() -> str:
+    try:
+        elements.append(create_fire())
+        elements.append(create_water())
+    except AttributeError as e:
+        print(f"Error found: {e}")
+    return (
+        f"Strength potion brewed with '{elements[2]}' and"
+        f" '{elements[3]}'"
+    )
