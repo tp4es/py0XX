71418fa93a92862679df8a5ee12292b4a006238f
Tiago_laptop <tide-oli@student.42madrid.com>
2026-05-27 18:19:10 +0200

    distillation_1:done

--- py06/alchemy/__init__.py
+++ py06/alchemy/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
 from .elements import create_air  # noqa: F401
+from .potions import strength_potion as original_strength
+from .potions import healing_potion as special_heal
--- py06/ft_distillation_1.py
+++ py06/ft_distillation_1.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env	python3
 
+import alchemy
+
+
+def create_potions() -> None:
+    print("=== Potions Service===\n")
+    try:
+        print(f"Trying healing potion:\n{alchemy.special_heal()}")
+        print(f"Trying strength potion:\n{alchemy.original_strength()}")
+    except Exception as e:
+        print(f"Error found: {e}")
+
+
+if __name__ == "__main__":
+    create_potions()
