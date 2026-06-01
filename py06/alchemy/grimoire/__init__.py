aff326c8a7487265d118478db977ce0893c30912
Tiago_laptop <tide-oli@student.42madrid.com>
2026-06-01 19:33:26 +0200

    done

--- py06/alchemy/grimoire/__init__.py
+++ py06/alchemy/grimoire/__init__.py
@@ -1,8 +1,5 @@
 from .light_spellbook import light_spell_record
 from .light_validator import validate_ingredients as light_validate_ingredients
-from .dark_spellbook import dark_spell_record
-from .dark_validator import validate_ingredients as dark_validate_ingredients
 
 
-__all__ = ["light_spell_record", "light_validate_ingredients",
-           "dark_spell_record", "dark_validate_ingredients"]
+__all__ = ["light_spell_record", "light_validate_ingredients"]
