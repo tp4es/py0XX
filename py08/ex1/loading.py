43d310dd911b47bc3f3baf9bf4fecef51c200d9e
Tiago_laptop <tide-oli@student.42madrid.com>
2026-06-13 22:21:30 +0200

    prev

--- py08/ex1/loading.py
+++ py08/ex1/loading.py
@@ -13,12 +13,18 @@ def check_deps(deps: list[str]) -> list[str]:
         except ImportError as e:
             print(f"{dep.capitalize()} module NOT FOUND.")
             missing.append(dep)
+    if len(missing) > 0:
+        print("1- Install using pip:\n  pip install -r requirements.txt")
+        print("2- Install using Poetry:\n  poetry install")
     return (ok, missing)
 
 
 def main() -> None:
-    with open("requirements.txt", "r") as read:
-        modules: list[str] = read.readline().splitlines()
+    with open("requierements.txt", "r") as read:
+        lines: list[str] = read.readlines()
+    modules = list()
+    for line in lines:
+        modules.append(line.strip(" ").strip("\n"))
     check_deps(modules)
 
 
