a4834d652452158f9c5b3fc0b58b0c5c9ba79693
Tiago_laptop <tide-oli@student.42madrid.com>
2026-06-16 02:48:34 +0200

    ex2

--- py08/ex2/oracle.py
+++ py08/ex2/oracle.py
@@ -1,1 +1,152 @@
+import os
 
+
+def load_env():
+    pass
+
+
+def check_security():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        exit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        SystemExit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        exit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        SystemExit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        exit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        SystemExit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        exit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
+
+
+def load_env():
+    pass
+
+
+def main():
+    try:
+        from dotenv import load_dotevent  # type: ignore
+    except Exception:
+        print("Error: 'python-dotenv' package is missing.")
+        print("Install: pip install python-dotenv")
+        SystemExit
+    print("Continue")
+
+
+if __name__ == "__main__":
+    main()
