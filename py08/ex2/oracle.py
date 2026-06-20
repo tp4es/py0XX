9404090057908f2de544f2af38906757ef81e5d5
Tiago_laptop <tide-oli@student.42madrid.com>
2026-06-20 21:19:05 +0200

    almost

--- py08/ex2/oracle.py
+++ py08/ex2/oracle.py
@@ -2,53 +2,78 @@ import os
 import sys
 
 
-def load_env() -> list[bool, list[tuple[str, str]]]:
-    env: tuple = ()
+def load_env() -> tuple[bool, list[tuple[str, str]]]:
+
     try:
         import dotenv as de  # type: ignore
         de.load_dotenv()
-        mode: str = os.getenv("MATRIX_MODE")
-        db: str = os.getenv("DATABASE_URL")
-        api_key: str = os.getenv("API_KEY")
-        log_level: str = os.getenv("LOG_LEVEL")
-        zion_edp: str = os.getenv("ZION_ENDPOINT")
-        return (True, )
+        env: list = []
+        mode: tuple = ("mode", os.getenv("MATRIX_MODE"))
+        db: tuple = ("db", os.getenv("DATABASE_URL"))
+        api_key: tuple = ("api_key", os.getenv("API_KEY"))
+        log_level: tuple = ("log_level", os.getenv("LOG_LEVEL"))
+        zion_edp: tuple = ("zion_edp", os.getenv("ZION_ENDPOINT"))
+        env.append(mode)
+        env.append(db)
+        env.append(api_key)
+        env.append(log_level)
+        env.append(zion_edp)
+        return (True, env)
     except Exception:
         print("Error: 'python-dotenv' package is missing.")
         print("Install: pip install python-dotenv")
         return (False, None)
 
 
-def get_override():
-    if len(sys.argv > 1):
-        return (True)
+def get_override() -> bool:
+    override: bool = False
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            try:
+                data: tuple = (sys.argv[i].split("=", 1))
+                os.environ[data[0]] = data[1]
+                print(data[0])
+                print(data[1])
+            except Exception as e:
+                print(f"Error split: {e}")
+        override = True
+    return (override)
 
 
-def check_security() -> list[]:
+def check_security() -> list[str]:
     path_git = ".gitignore"
     path_env = ".env"
+    info: list = []
     try:
         if os.path.exists(path_git):
-            print("[OK] No hardcoded secrets detected.")
+            git: str = print("[OK] No hardcoded secrets detected.")
         else:
-            print("[CAUTION] '.gitignore' file not found.")
+            git: str = print("[CAUTION] File '.gitignore' not found.")
         if os.path.exists(path_env):
-            print("[OK] '.env' file properly configured.")
+            env: str = print("[OK] File '.env' properly configured.")
         else:
-            print("[CAUTION] '.env.' file not found.")
+            env: str = print("[CAUTION] File '.env.' not found.")
         if get_override():
             print("[OK] Production overrides activated.")
+        info.append(git)
+        info.append(env)
+        return (info)
     except Exception as e:
-        print(f"Erro caught: {e}")
+        return (f"Erro caught_security check: {e}")
 
 
 def main():
+    print("ORACLE STATUS: Reading the Matrix...\n")
     try:
-        if load_env():
-
+        loaded = load_env()
+        if loaded[0]:
             print("Configuration loaded:")
-            print(f"Mode: {e}")
-            print(f"Database: {db}")
+            for a in loaded[1]:
+                print(f"{a[0].upper()}: {a[1].capitalize()}")
+        print("\nEnviroment security check:")
+        info = check_security()
+        for a in info:
+            print(a)
     except Exception as e:
         print(f"Error caught: {e}")
 
